8b73f03e08ae57fdc171b8abbca89d5c27f34c85
rahulatnimap <rahul.choudhary@nimapinfotech.com>
2025-01-15 04:26:42 -0800

    update

---
+++ timerexamples/src/components/IntervalOut.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const IntervalOut = () => {
+  return (
+    <div>IntervalOut</div>
+  )
+}
+
+export default IntervalOut
---
+++ timerexamples/src/components/TimeOut.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const TimeOut = () => {
+  return (
+    <div>TimeOut</div>
+  )
+}
+
+export default TimeOut
